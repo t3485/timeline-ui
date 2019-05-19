4b57a6dd2380c4c88e645acafb5bbb6fa1598896
flowers <sghmenbs@163.com>
2019-05-19 20:48:02 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,29 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <el-container class="container">
+    <el-header>
+      <nav>     
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </nav>      
+    </el-header>
+    <el-main>
+      <router-view/>   
+    </el-main>
+    <el-footer>
+      
+     </el-footer>
+  </el-container>
 </template>
 <style lang="stylus">
+body
+  margin 0
+  padding 0
 #app
   font-family 'Avenir', Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-
-#nav
-  padding 30px
-  a
-    font-weight bold
-    color #2c3e50
-    &.router-link-exact-active
-      color #42b983
+nav
+  padding 20px 80px 
+.container  
+  min-height 100vh
 </style>
